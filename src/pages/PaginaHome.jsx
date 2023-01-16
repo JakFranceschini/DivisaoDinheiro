a39cbec1f0dbf43ee00db1410f145622c13a8e84
JakFranceschini <jakson.franceschini@gmail.com>
2023-01-16 19:04:55 -0300

    adicionado novas funcionalidades

--- src/pages/PaginaHome.jsx
+++ src/pages/PaginaHome.jsx
@@ -6,13 +6,24 @@ import { useEffect } from "react";
 
 function PaginaHome() {
 
-
     useEffect(() => {
       let botaoCalcular = document.getElementById('botaoCalcular');
+      let containerPorcentagens = document.getElementById('containerPorcentagens');
+      containerPorcentagens.style.display = 'none';
+      let containerResultados = document.getElementById('containerResultados');
+      containerResultados.style.display = 'none';
+      let botaoVerResultados = document.getElementById('botaoVerResultados');
+      botaoVerResultados.style.display = 'none';
+      let botaoZerar = document.getElementById('botaoZerar');
+      botaoZerar.style.display = 'none';
 
       botaoCalcular.addEventListener('click', function(){
         botaoCalcular.style.display = 'none';
-
+        containerPorcentagens.style.display = 'flex';
+        botaoVerResultados.style.display = 'flex';
+        botaoVerResultados.style.alignItems = 'center';
+        botaoVerResultados.style.justifyContent = 'center';
+        
         document.getElementById('tituloH2').innerHTML = 'Total liquido';
         document.getElementById('tituloH3').innerHTML = 'Total liquido do mês';
 
@@ -26,9 +37,52 @@ function PaginaHome() {
         labelResultado.style.alignItems = 'center';
         labelResultado.style.justifyContent = 'center';
       });
+
+      botaoVerResultados.addEventListener('click', function(){
+        botaoVerResultados.style.display = 'none';
+        containerResultados.style.display = 'flex';
+        botaoZerar.style.display = 'flex';
+        botaoZerar.style.alignItems = 'center';
+        botaoZerar.style.justifyContent = 'center';
+
+        const inputReceita = parseInt(document.getElementById('inputReceita').value);
+        const inputDespesa = parseInt(document.getElementById('inputDespesa').value);
+        const resultado = inputReceita - inputDespesa;
+
+        const inputPorecentagemGastos = parseInt(document.getElementById('inputPorecentagemGastos').value);
+        const labelGastos = document.getElementById('labelGastos');
+        const resultadoGastos = resultado * inputPorecentagemGastos / 100;
+        labelGastos.innerHTML = resultadoGastos;
+
+        const inputPorecentagemInvestimentos = parseInt(document.getElementById('inputPorecentagemInvestimentos').value);
+        const labelInvestimentos = document.getElementById('labelInvestimentos');
+        const resultadoInvestimentos = resultado * inputPorecentagemInvestimentos / 100;
+        labelInvestimentos.innerHTML = resultadoInvestimentos;
+
+        const inputPorecentagemReserva = parseInt(document.getElementById('inputPorecentagemReserva').value);
+        const labelReserva = document.getElementById('labelReserva');
+        const resultadoReserva = resultado * inputPorecentagemReserva / 100;
+        labelReserva.innerHTML = resultadoReserva;
+      });
+
+      botaoZerar.addEventListener('click', function(){
+        botaoZerar.style.display = 'none';
+        containerPorcentagens.style.display = 'none';
+        containerResultados.style.display = 'none';
+        botaoCalcular.style.display = 'flex';
+        botaoCalcular.style.alignItems = 'center';
+        botaoCalcular.style.justifyContent = 'center';
+        
+        document.getElementById('tituloH2').style.display = 'none';
+        document.getElementById('tituloH3').style.display = 'none';
+        document.getElementById('labelResultado').style.display = 'none';
+        document.getElementById('inputReceita').value = '';
+        document.getElementById('inputDespesa').value = '';
+        document.getElementById('inputPorecentagemGastos').value = '';
+        document.getElementById('inputPorecentagemInvestimentos').value = '';
+        document.getElementById('inputPorecentagemReserva').value = '';
+      });
     });
-
-
 
     return (
       <div className="paginaHome">
@@ -59,6 +113,45 @@ function PaginaHome() {
               <input id="inputDespesa" type="number"/>
             </div>
           </div>
+
+          <div id="containerPorcentagens">
+            <div className="containerPorcentagemGastos">
+              <h2>Gastos</h2>
+              <h3>Digite a porcentagem para gastos</h3>
+              <input id="inputPorecentagemGastos" type="number"/>
+            </div>
+            <div className="containerPorcentagemInvestimentos">
+              <h2>Investimentos</h2>
+              <h3>Digite a porcentagem para Investimentos</h3>
+              <input id="inputPorecentagemInvestimentos" type="number"/>
+            </div>
+            <div className="containerPorcentagemReserva">
+              <h2>Reserva de emergência</h2>
+              <h3>Digite a porcentagem para reserva de emergência</h3>
+              <input id="inputPorecentagemReserva" type="number"/>
+            </div>
+          </div>
+
+          <button id="botaoVerResultados">Ver Resultados</button>
+
+          <div id="containerResultados">
+            <div className="containerGastos">
+              <h2>Gastos</h2>
+              <h3>Total destinado para gastos</h3>
+              <label id="labelGastos"></label>
+            </div>
+            <div className="containerInvestimentos">
+              <h2>Investimentos</h2>
+              <h3>Total destinado para Investimentos</h3>
+              <label id="labelInvestimentos"></label>
+            </div>
+            <div className="containerReserva">
+              <h2>Reserva de emergência</h2>
+              <h3>Total destinado para reserva de emergência</h3>
+              <label id="labelReserva"></label>
+            </div>
+          </div>
+          <button id="botaoZerar">Novo calculo</button>
         </section>
       </div>
     );
